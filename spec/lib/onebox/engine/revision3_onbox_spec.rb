763969654306e627cb1ab4c73bcb61d858758485
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-16 21:24:15 -0700

    Adding behavior testing for the matcher on all engines

--- spec/lib/onebox/engine/revision3_onbox_spec.rb
+++ spec/lib/onebox/engine/revision3_onbox_spec.rb
@@ -6,7 +6,7 @@ describe Onebox::Engine::Revision3Onebox do
     fake(link, response("revision3.response"))
   end
 
-  it_behaves_like "engines"
+  it_behaves_like "an engine"
 
   describe "#to_html" do
     let(:html) { described_class.new(link).to_html }
