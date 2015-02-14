4fce1110c647cdb704d094b4def6d807c3b02543
billy3321 <billy3321@gmail.com>
2015-02-14 21:21:36 +0800

    now test can work.

--- spec/lib/onebox/engine/ptt_onebox_spec.rb
+++ spec/lib/onebox/engine/ptt_onebox_spec.rb
@@ -1,52 +1,53 @@
 require "spec_helper"
 
 describe Onebox::Engine::PttOnebox do
-
-  include_context "engines"
-
-  describe "gossiping work" do
-    before(:all) do
-      @link = "https://www.ptt.cc/bbs/Gossiping/M.1421846278.A.824.html"
-    end
-
-    it "#title" do
-      expect(html).to include("清查不當黨產展覽展件資料公開")
-    end
-
-    it "#content" do
-      expect(html).to include("清查不當黨產 捍衛國家資產")
-    end
-
-    it "comment author" do
-      expect(html).to include("cttw19")
-    end
-
-    it "comment content" do
-      expect(html).to include("國民黨不倒 台灣不會好")
-    end
+  before(:all) do
+    FakeWeb.register_uri(:get, 'https://www.ptt.cc/bbs/PublicIssue/M.1421063009.A.F39.html', body: response('ptt'), content_type: 'text/html')
+    FakeWeb.register_uri(:post, 'https://www.ptt.cc/ask/over18', body: response('ptt-gossiping'), content_type: 'text/html', parameters: {from: '/bbs/Gossiping/M.1421846278.A.824.html', yes: 'yes'})
   end
 
   describe "others work" do
     before(:all) do
-      @link = "https://www.ptt.cc/bbs/PublicIssue/M.1421063009.A.F39.html"
+      @preview = Onebox.preview("https://www.ptt.cc/bbs/PublicIssue/M.1421063009.A.F39.html")
     end
 
-    it_behaves_like "an engine"
-
     it "#title" do
-      expect(html).to include("剝蕉案(青果社事件)")
+      expect(@preview.to_s).to include("剝蕉案(青果社事件)")
     end
 
     it "#content" do
-      expect(html).to include("國父紀念館興建(跟農民勒索！)和外貿協會成立，青果社均贊助龐大費用")
+      expect(@preview.to_s).to include("國父紀念館興建(跟農民勒索！)和外貿協會成立，青果社均贊助龐大費用")
     end
 
     it "comment author" do
-      expect(html).to include("aa789")
+      expect(@preview.to_s).to include("aa789")
     end
 
     it "comment content" do
-      expect(html).to include("好文不推嗎？")
+      expect(@preview.to_s).to include("好文不推嗎？")
+    end
+  end
+
+
+  describe "gossiping work" do
+    before(:all) do
+      @preview = Onebox.preview("https://www.ptt.cc/bbs/Gossiping/M.1421846278.A.824.html")
+    end
+
+    it "#title" do
+      expect(@preview.to_s).to include("清查不當黨產展覽展件資料公開")
+    end
+
+    it "#content" do
+      expect(@preview.to_s).to include("清查不當黨產 捍衛國家資產")
+    end
+
+    it "comment author" do
+      expect(@preview.to_s).to include("cttw19")
+    end
+
+    it "comment content" do
+      expect(@preview.to_s).to include("國民黨不倒 台灣不會好")
     end
   end
 end
