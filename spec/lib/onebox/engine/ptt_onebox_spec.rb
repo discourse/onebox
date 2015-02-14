03170369415cfb00d2fad866451008e5ec8e8433
billy3321 <billy3321@gmail.com>
2015-02-15 01:39:46 +0800

    fix test name

--- spec/lib/onebox/engine/ptt_onebox_spec.rb
+++ spec/lib/onebox/engine/ptt_onebox_spec.rb
@@ -22,11 +22,11 @@ describe Onebox::Engine::PttOnebox do
       expect(html).to include("國父紀念館興建(跟農民勒索！)和外貿協會成立，青果社均贊助龐大費用")
     end
 
-    it "comment author" do
+    it "#comment author" do
       expect(html).to include("aa789")
     end
 
-    it "comment content" do
+    it "#comment content" do
       expect(html).to include("好文不推嗎？")
     end
   end
@@ -48,11 +48,11 @@ describe Onebox::Engine::PttOnebox do
       expect(html).to include("清查不當黨產 捍衛國家資產")
     end
 
-    it "comment author" do
+    it "#comment author" do
       expect(html).to include("cttw19")
     end
 
-    it "comment content" do
+    it "#comment content" do
       expect(html).to include("國民黨不倒 台灣不會好")
     end
   end
