31d01651c4e3cded7c623bf2bd1d376b76c45e69
Jess Portnoy <jess.portnoy@kaltura.com>
2016-01-08 13:33:02 +0000

    fixed tests

--- spec/lib/onebox/engine/kaltura_onebox_spec.rb
+++ spec/lib/onebox/engine/kaltura_onebox_spec.rb
@@ -1,12 +1,18 @@
 require "spec_helper"
 describe Onebox::Engine::KalturaOnebox do
+  before do
+    fake("http://www.kaltura.com/tiny/sj0h8", response("kaltura"))
+  end
+  it "Has embedIframeJs inlcude" do
+    expect(Onebox.preview('http://www.kaltura.com/tiny/sj0h8').to_s).to match(/\<script src=".*\/embedIframeJs\//)
+  end
+  it "Has kWidget.embed" do
+    expect(Onebox.preview('http://www.kaltura.com/tiny/sj0h8').to_s).to match(/kWidget\.embed/)
+  end
   it "Has entry ID" do
     expect(Onebox.preview('http://www.kaltura.com/tiny/sj0h8').to_s).to match(/"entry_id": "([0-9a-z_]+)"/)
   end
   it "Has uiconf ID" do
-    expect(Onebox.preview('http://www.kaltura.com/index.php/extwidget/preview/partner_id/1829791/uiconf_id/32334692/entry_id/1_ddrwr3yz/embed/auto?&flashvars[streamerType]=auto').to_s).to match(/"uiconf_id": ([0-9]+)/)
-  end
-  it "Has embedIframeJs" do
-    expect(Onebox.preview('http://www.kaltura.com/index.php/extwidget/preview/partner_id/1829791/uiconf_id/32334692/entry_id/1_ddrwr3yz/embed/auto?&flashvars[streamerType]=auto').to_s).to match(/"<script src=".*\/embedIframeJs\//)
+    expect(Onebox.preview('http://www.kaltura.com/tiny/sj0h8').to_s).to match(/"uiconf_id": ([0-9]+)/)
   end
 end
