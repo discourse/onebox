d428ff34fef20b6d07ca7e1f221446697e79865d
Roman Rizzi <rizziromanalejandro@gmail.com>
2019-02-12 22:05:48 -0300

    Adds kaltura support using OpenGraph

---
+++ lib/onebox/engine/kaltura_onebox.rb
@@ -0,0 +1,31 @@
+module Onebox
+  module Engine
+    class KalturaOnebox
+      include Engine
+      include StandardEmbed
+
+      always_https
+      matches_regexp(/^https?:\/\/[a-z0-9]+\.kaltura\.com\/id\/[a-zA-Z0-9]+/)
+
+      def preview_html
+        og = get_opengraph
+
+        <<~HTML
+          <img src="#{og[:image_secure_url]}" width="#{og[:video_width]}" height="#{og[:video_height]}" >
+        HTML
+      end
+
+      def to_html
+        og = get_opengraph
+
+        <<~HTML
+          <iframe src="#{og[:video_secure_url]}"
+                  width="#{og[:video_width]}" height="#{og[:video_height]}"
+                  frameborder='0'
+                  allowfullscreen >
+          </iframe>
+        HTML
+      end
+    end
+  end
+end
