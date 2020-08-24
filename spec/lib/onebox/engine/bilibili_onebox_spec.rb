f87f8c7bde4017e8fd2272cd4bf6b2cfc3a91742
fafa_junhe <842355358@qq.com>
2020-08-25 01:37:19 +0800

    Update bilibili_onebox_spec.rb

--- spec/lib/onebox/engine/bilibili_onebox_spec.rb
+++ spec/lib/onebox/engine/bilibili_onebox_spec.rb
@@ -4,9 +4,9 @@ require "spec_helper"
 
 describe Onebox::Engine::BiliBiliOnebox do
 
-  let(:link) {"https://www.bilibili.com/video/BV1Js411o76u"}
-  let(:avlink) {"https://www.bilibili.com/video/av810872"} #just same to bv link
-  let(:outlink) {"https://player.bilibili.com/player.html?aid=810872&amp;page=1&amp;as_wide=1"}
+  let(:link) { "https://www.bilibili.com/video/BV1Js411o76u" }
+  let(:avlink) { "https://www.bilibili.com/video/av810872" } #just same to bv link
+  let(:outlink) { "https://player.bilibili.com/player.html?aid=810872&amp;page=1&amp;as_wide=1" }
   let(:html) { described_class.new(link).to_html }
   let(:avhtml) { described_class.new(avlink).to_html }
 
