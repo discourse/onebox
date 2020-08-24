4651010607ffffbe4df63f7ae91f03fa7561f72d
fafa_junhe <842355358@qq.com>
2020-08-25 01:04:29 +0800

    Create bilibili_onebox_spec.rb

---
+++ spec/lib/onebox/engine/bilibili_onebox_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe Onebox::Engine::BiliBiliOnebox do
+
+  let(:link) {"https://www.bilibili.com/video/av810872"}
+  let(:html) { described_class.new(link).to_html }
+
+  describe "#to_html" do
+
+    # player.bilibili.com already have a window for iframe
+    it "has the url" do
+      expect(html).to include("https://player.bilibili.com")
+    end
+end
