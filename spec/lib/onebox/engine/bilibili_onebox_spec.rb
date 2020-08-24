014fed931f8dc0b5bc64c82e6152d939e80f9407
fafa_junhe <842355358@qq.com>
2020-08-25 01:17:34 +0800

    Update bilibili_onebox_spec.rb

--- spec/lib/onebox/engine/bilibili_onebox_spec.rb
+++ spec/lib/onebox/engine/bilibili_onebox_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 describe Onebox::Engine::BiliBiliOnebox do
 
-  let(:link) {"https://www.bilibili.com/video/av810872"}
+  let(:link) { "https://www.bilibili.com/video/av810872" }
   let(:html) { described_class.new(link).to_html }
 
   describe "#to_html" do
