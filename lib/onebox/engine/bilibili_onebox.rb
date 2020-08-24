639274a8817841550a136b427b46fd9289ae9d08
fafa_junhe <842355358@qq.com>
2020-08-25 00:23:57 +0800

    Syntax fix

--- lib/onebox/engine/bilibili_onebox.rb
+++ lib/onebox/engine/bilibili_onebox.rb
@@ -11,37 +11,35 @@ module Onebox
         "<iframe src=\"https://player.bilibili.com/player.html?aid=#{id()}&amp;page=1&amp;as_wide=1\" frameborder=\"0\" width=\"640\" height=\"430\" allowfullscreen=\"true\" seamless=\"seamless\" sandbox=\"allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox allow-presentation\"></iframe>"
       end
 
-      
       private
       # decode copied from  https://github.com/ShiSheng233/bili_BV/blob/master/biliBV/__init__.py
       @@key = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
       @@dic = {}
+
       for a in 0..57
-        @@dic[@@key[a]]=a
+        @@dic[@@key[a]] = a
       end
-      @@s=[11,10,3,8,4,6]
-      @@xor=177451812
-      @@add=8728348608
+
+      @@s = [11, 10, 3, 8, 4, 6]
+      @@xor = 177451812
+      @@add = 8728348608
+
       def decode(in_)
-        r=0
+        r = 0
         for a in 0..5
-          @@r = @@dic[@@in_[@@s[a]]]*58**a + @@r
+          @@r = @@dic[@@in_[@@s[a]]] * 58 ** a + @@r
         end
-      return (@@r-@@add)^@@xor
+      (@@r - @@add) ^ @@xor
       end
 
       def id
         match = @url.match(/(bv|av)(.*)/i)
-        if match[1] == "BV" or match[1] == "bv"
-          return decode(match[1]+match[2])
+        if match[1] == "BV" || match[1] == "bv"
+          decode(match[1] + match[2])
         else
-          return match[2]
-          
+          match[2]
         end
       end
-
-
-
     end
   end
 end
